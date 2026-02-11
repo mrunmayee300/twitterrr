00bf3c53f1cf11b023a68f893bd9b66f26141481
mrunmayee300 <mrunmayeesangode@gmail.com>
2026-02-11 12:51:16 +0530

    add mobile as normal folder

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack />;
+}
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Stack } from "expo-router";
+import "../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StatusBar } from "expo-status-bar";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <StatusBar style="dark" />
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
+}
